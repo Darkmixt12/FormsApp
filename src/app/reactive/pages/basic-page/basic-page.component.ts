fa89e9cadd1cef2e48a1207f0effd3641fe61c4b
Steven Muñoz <darkmixt@gmail.com>
2023-10-05 17:54:44 -0600

    new validators added

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -17,15 +17,17 @@ export class BasicPageComponent {
   //})
 
   public myForm: FormGroup = this.fb.group({
-    name: [''],
-    price: [0],
-    inStorage: [0],
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
   })
 
   constructor( private fb: FormBuilder){}
 
 
   onSave():void{
+
+    if(this.myForm.invalid) return
     console.log(this.myForm.value)
   }
 
