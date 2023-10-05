209a49fbd6cdb85bc57bfe8d760a4a31ad5aadad
Steven Muñoz Vega <Darkmixt@gmail.com>
2023-10-04 22:29:13 -0600

    Form vinculation with HTML added

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   ]
 })
 export class BasicPageComponent {
+
+
+ // public myForm: FormGroup = new FormGroup({
+//
+  //  name: new FormControl(''),
+  //  price: new FormControl(0),
+  //  inStorage: new FormControl(0)
+  //})
+
+  public myForm: FormGroup = this.fb.group({
+    name: [''],
+    price: [0],
+    inStorage: [0],
+  })
+
+  constructor( private fb: FormBuilder){}
+
+
+  onSave():void{
+    console.log(this.myForm.value)
+  }
+
 
 }
